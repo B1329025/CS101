54e3c7837e81d7e2205da77cfd18d002eac772c6
B1329025 <B1329025@cgu.edu.tw>
2024-12-05 14:39:09 +0800

    Update main6.c

--- EX#3/main6.c
+++ EX#3/main6.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+   int i =3000;
+   int a;
+   if (i<=1500){
+       printf("70 元");
+   }
+   
+   else{
+       if(i%100 !=0){
+           a= (((i-1500)/100)+1)*10;
+           printf("%d 元",70+a);
+       }
+       else{
+           a=((i-1500)/100)*10;
+           printf("%d 元",70+a);
+       }
+   }
+}   
