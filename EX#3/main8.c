884d195ec81c39dcf9535554b5ef88571cbabacd
B1329025 <B1329025@cgu.edu.tw>
2024-12-05 14:56:56 +0800

    Update main8.c

--- EX#3/main8.c
+++ EX#3/main8.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+  int x = 9;
+  int y = 9;
+  int z = 1;
+  int i ;
+  if(x<0){
+      i=((x*-1)*100+(y*10)+z);
+      printf("%d",i*-1);
+  }
+  else{
+    i=((x*100)+(y*10)+z);
+    printf("%d",i); 
+  }
+}   
