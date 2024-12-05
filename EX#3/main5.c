fc2bfb290bef2f2bae9663f8d38592765d4f571c
B1329025 <B1329025@cgu.edu.tw>
2024-12-05 14:31:40 +0800

    Update main5.c

--- EX#3/main5.c
+++ EX#3/main5.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+   int i =300;
+   int a;
+   if (i<=30){
+       printf("free");
+   }
+   else if(i >240 ){
+       printf("240元");
+   }
+   else{
+       if(i%30 !=0){
+           a= ((i/30)+1)*30;
+           printf("%d 元",a);
+       }
+       else{
+           printf("%d 元",a);
+       }
+   }
+}   
