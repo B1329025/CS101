15a9502ac0f1afa338f0c5fcd78425a4a2ab0a64
B1329025 <B1329025@cgu.edu.tw>
2024-12-10 20:27:06 +0800

    Update main1.c

--- Hw#5/main1.c
+++ Hw#5/main1.c
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int rows = 7;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows + i - 1; j++) {
+            if (j <= rows - i) {
+                cout << " "; 
+            } else if ((j - (rows - i)) % 2 == 1) {
+                cout << i; 
+            } else {
+                cout << " "; 
+            }
+        }
+        cout <<"\n"; 
+    }
+
+    return 0;
+}
