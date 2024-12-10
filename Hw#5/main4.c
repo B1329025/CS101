96966ad8fb0cf01b4db065fd74ec3f909a112cdd
B1329025 <B1329025@cgu.edu.tw>
2024-12-10 22:06:43 +0800

    Update main4.c

--- Hw#5/main4.c
+++ Hw#5/main4.c
@@ -1,43 +1,10 @@
 #include <stdio.h>
-#include <math.h>
-
-int swap_units_and_thousands(int num) {
-    int digits[100], length = 0, temp = num;
-
-    while (temp > 0) {
-            digits[length++] = temp % 10;
-            temp /= 10;
-    }
-
-    int temp_digit = digits[0];        
-    digits[0] = digits[3];             
-    digits[3] = temp_digit;            
-    int result = 0;
-    for (int i = length - 1; i >= 0; i--) {
-        result = result * 10 + digits[i];
-    }
-
-    return result;
-}
-
-int main() {
-    int num =12345;
-    int digits[100];
-    if(num>=1000){
-        int result = swap_units_and_thousands(num);
-        printf("交換後的數字為：%d\n", result);
-    }
-    else {
-        for(int j=0;j<4;j++){
-            digits[j] = num % 10;
-            num /= 10;
-        }      
-        digits[3] = digits[0];             
-        digits[0] = 0;
-        for (int i = 3; i >=0; i--) {
-            printf("%d",digits[i]);
-        }
-        
-    }
+int main(){
+    int i = 1234567;
+    int thousandplace = i / 1000 % 10;
+    int unitplace = i%10;
+    i -=thousandplace*1000+unitplace;
+    i +=unitplace*1000+thousandplace;
+    printf("%d",i);
     return 0;
 }
