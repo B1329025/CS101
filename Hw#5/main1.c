a4d4003ed60c674b010802b5cbfa3812a33d3667
B1329025 <B1329025@cgu.edu.tw>
2024-12-10 20:28:51 +0800

    Update main1.c

--- Hw#5/main1.c
+++ Hw#5/main1.c
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-    int rows = 7;
+int main()
+{
+   int rows = 7;
 
     for (int i = 1; i <= rows; i++) {
         for (int j = 1; j <= rows + i - 1; j++) {
             if (j <= rows - i) {
-                cout << " "; 
+                printf(" "); 
             } else if ((j - (rows - i)) % 2 == 1) {
-                cout << i; 
+                printf("%d",i); 
             } else {
-                cout << " "; 
+                printf(" "); 
             }
         }
-        cout <<"\n"; 
+        printf("\n"); 
     }
 
     return 0;
