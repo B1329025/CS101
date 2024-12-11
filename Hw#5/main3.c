b1a390ba4b1625db7a75723e58bbaedfbfdcaecd
B1329025 <B1329025@cgu.edu.tw>
2024-12-11 23:02:52 +0800

    Update main3.c

--- Hw#5/main3.c
+++ Hw#5/main3.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-
-
 int main() {
-    int a,b;
-    for(int i=1;i<=81;i++){
-    a=(i-1)/9+1;
-    b=(i-1)%9+1;
-    printf("%d ",a*b);
-    
-    if(b==9){
-        printf("\n");
+    int a, b;
+    for (int i = 1; i <= 81; i++) {
+        a = (i - 1) / 9 + 1;
+        b = (i - 1) % 9 + 1;
+
+        // 使用 %2d 和 %-2d 讓數字對齊，乘法式子排成整齊的列
+        printf("%2d * %-1d = %2d  ", a, b, a * b);
+
+        if (b == 9) {
+            printf("\n"); // 每 9 個數字換行
+        }
     }
-    }
-    
+
+    return 0;
 }
