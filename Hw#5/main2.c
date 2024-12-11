59db8bdb03e54e1d9f2d5e2b7d0ac734f3b4fd52
B1329025 <B1329025@cgu.edu.tw>
2024-12-11 23:25:27 +0800

    Update main2.c

--- Hw#5/main2.c
+++ Hw#5/main2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    double pi = 0.0;
+    int sign = 1; 
+    int terms = 0; 
+
+    while (!(pi >= 3.141585 && pi < 3.141595)) { 
+        pi += sign * (4.0 / (2 * terms + 1));
+        sign = -sign; 
+        terms++;
+    }
+
+    printf("所需項數為: %d\n", terms);
+
+    return 0;
+}
