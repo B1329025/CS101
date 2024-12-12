0007105ec833b200ae95f9b636d069c602ed4b85
B1329025 <B1329025@cgu.edu.tw>
2024-12-12 14:43:15 +0800

    Update main1.c

--- Ex#4/main1.c
+++ Ex#4/main1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=9;
+    int spc=n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=spc+i;j++){
+            if(j<=spc){
+                printf(" ");
+            }
+            else{
+                printf("%d ",i);
+            }
+        }
+        printf("\n");
+        spc--;
+    }
+}
 
