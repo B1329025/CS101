895cc4d3f1bef671ba53d5b12b7a09218eec2014
B1329025 <B1329025@cgu.edu.tw>
2024-12-12 22:30:54 +0800

    Update main3.c

--- Hw#5/main3.c
+++ Hw#5/main3.c
@@ -5,7 +5,7 @@ int main() {
     for (int i = 1; i <= 81; i++) {
         a = (i - 1) / 9 + 1;
         b = (i - 1) % 9 + 1; 
-        printf("%2d * %-1d = %2d  ", a, b, a * b);
+        printf("%d*%d=%d\t", a, b, a * b);
         if (b == 9) {
             printf("\n"); 
         }
